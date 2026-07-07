b93d3399706b7d0187d235bf15b920ff04633cc9
Cheryala Naveen <NAVEENCHARY2004@GMAIL.COM>
2026-07-07 10:55:15 +0530

    Time: 8 ms (8.63%), Space: 46 MB (33.93%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+       String rev = new StringBuilder(s).reverse().toString();
+        return rev.equals(s);
+}
+}
